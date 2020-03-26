c26a7beed0037432460d42baa7db5e84411aa10b
souzakayque <52177811+pereirakayque@users.noreply.github.com>
2020-03-26 10:12:05 -0300

    começando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Logon from './pages/Logon';
+
+import './global.css'
+
+function App() {
+
+  return (
+
+    <Logon />
+    
+  );
+}
+
+export default App;
